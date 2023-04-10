55b737829328a86b40a022428d4ab9cdb3001b39
HaroldF415 <harold.f415@gmail.com>
2023-04-10 11:57:50 -0400

    locations controller updated

---
+++ controllers/locations.controller.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const locations = express.Router();
+const locationsArr = require("../models/location.model");
+
+const locationHandler = (req, res) => {};
+
+locations.get();
