deb72e8ab9f94e55099e3bf0e342754c7116c056
joshuanelsondev <joshuanelsondev@gmail.com>
2023-04-10 16:55:52 -0400

    Complete Lab

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 // DEPENDENCIES
 const express = require("express");
-
+const locationsController = require("./controllers/locations.controller");
+const personsController = require("./controllers/persons.controller");
+const plansController = require("./controllers/plans.controller");
+const machinesController = require("./controllers/machines.controller");
+const specialEventsController = require("./controllers/special-events.controller");
 
 // CONFIGURATION
 const app = express();
@@ -10,6 +14,14 @@ app.get("/", (req, res) => {
     res.send("Hello, world!");
 })
 
+app.use("/locations", locationsController);
+app.use("/persons", personsController);
+app.use("/plans", plansController);
+app.use("/machines", machinesController);
+app.use("/special-events", specialEventsController);
 
+app.get("*", (req, res) => {
+    res.status(404).json({error: "Page Not Found"});
+});
 // EXPORT
 module.exports = app;
---
+++ controllers/locations.controller.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const locations = express.Router();
+const locationsArray = require("../models/location.model");
+
+locations.get("/", (req, res) => {
+    res.json(locationsArray);
+})
+
+locations.get("/:index", (req, res) => {
+    const { index } = req.params;
+    res.json(locationsArray[index]);
+})
+
+module.exports = locations;
