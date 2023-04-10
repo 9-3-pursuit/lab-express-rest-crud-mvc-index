b0bdc34bb398b94db5a82270196bc9983e14193b
amber-king <amberking@pursuit.org>
2023-04-10 14:48:09 -0400

    app.js updated w/ middlewear work & dependencies

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 // DEPENDENCIES
 const express = require("express");
+const locations = require("./controllers/locationsController")
+const machines = require("./controllers/machinesController")
+const persons = require("./controllers/personsController")
+const plans = require("./controllers/plansController")
+const specialEvents = require("./controllers/special-eventsController")
 
 // CONFIGURATION
 const app = express();
 
+
+
 // ROUTES
 app.get("/", (req, res) => {
-  res.send("Hello, world!");
+  res.send("Hello, world! 👋🏾");
 });
+
+//app.use()-Middlewear 
+app.use("/locations", locations)
+app.use("/machines", machines)
+app.use("/persons", persons)
+app.use("/plans", plans)
+app.use("/special-events", specialEvents)
 
 // EXPORT
 module.exports = app;
